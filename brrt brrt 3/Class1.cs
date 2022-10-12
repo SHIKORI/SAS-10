7044ad0c1dcb52d75783b5b8de387e2b1cba52c4
MILES FELICIANO <115053837+SHIKORI@users.noreply.github.com>
2022-10-12 23:31:49 +0800

    Add files via upload

---
+++ brrt brrt 3/Class1.cs
@@ -0,0 +1,51 @@
+﻿namespace brrt_brrt_3
+{
+        class Circle
+        {
+            public string color;
+            public double radius;
+            public const double PI = 3.14159;
+
+        public Circle(double r)
+            {
+                radius = r;
+            }
+
+        public Circle ()
+            {
+                radius = 0.0;
+            }
+
+             public double getArea
+            {
+                get
+                {
+                    return PI * radius * radius;
+                }
+
+            }
+            public double getCircumference
+            {
+                get
+                {
+                    return 2 * PI * radius;
+                }
+
+            } 
+            public double Radius
+            {
+                get
+                {
+                    return radius;
+                }
+
+                set
+                {
+                    if (value >= 0)
+                        radius = value;
+                }
+            }
+        }
+}
+
+
---
+++ brrt brrt 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using brrt_brrt_3;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Color of the circle: ");
+        string color = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("What is the circle’s radius: ");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        Circle ans = new Circle(radius);
+
+        Console.WriteLine("The color of the circle is: "+ color);
+        Console.WriteLine("The area of the circle is " + ans.getArea);
+        Console.WriteLine("The Circumference of the circle is " + ans.getCircumference);
+    }
+}
